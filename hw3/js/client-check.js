8fea44ba4be8ca841a66ee9f11ccbe6476dbaced
KolosovMax <KolosovMaxA@gmail.com>
2022-06-16 15:03:27 +0300

    Клиентская проверка, без отправки на сервер

--- hw3/js/client-check.js
+++ hw3/js/client-check.js
@@ -1,28 +1,21 @@
 'use strict';
 document.forms['files-loading'].addEventListener('submit', function (event){
     event.preventDefault();
-    let sizeCorrect;
     let maxSize = 0;
-    let nameCheck;
-    let typeCheck;
     for (let file of this.elements.foto.files){
         maxSize += file.size;
-        if (file.size <= 2097152) sizeCorrect= true
-        else {
-            document.getElementById('message').innerText = 'Некорректный размер файлов';
+        if (file.size >= 2097152){
+            document.getElementById('message').innerText = 'Некорректный размер файла';
             return;
             }
-        console.log(sizeCorrect);
-        if (file.type.includes('image')) typeCheck = true
-        else {
+        if (!file.type.includes('image')){
             document.getElementById('message').innerText = 'Некорректный тип файлов';
             return;
         }
-        if (file.name.trim().length) nameCheck = true
-        else {
+        if (!file.name.trim().length){
             document.getElementById('message').innerText = 'Некорректное имя фалов';
             return;
-        }console.log (sizeCorrect, typeCheck, nameCheck, maxSize)
+        }
         if (maxSize <20971520 ) document.getElementById('message').innerText = 'Файлы отправленны на сервер'
         else document.getElementById('message').innerText = 'Некорректный размер файлов'
     }
