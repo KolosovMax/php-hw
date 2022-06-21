45c300e1b7da5f9a50b6c8b1ca7de7057c02ebd3
KolosovMax <KolosovMaxA@gmail.com>
2022-06-21 12:50:02 +0300

    Серверная и клиентская проверка без подробнее

--- hw3/js/client-check.js
+++ hw3/js/client-check.js
@@ -1,22 +1,59 @@
 'use strict';
 document.forms['files-loading'].addEventListener('submit', function (event){
     event.preventDefault();
-    let maxSize = 0;
-    for (let file of this.elements.foto.files){
-        maxSize += file.size;
-        if (file.size >= 2097152){
-            document.getElementById('message').innerText = 'Некорректный размер файла';
-            return;
-            }
-        if (!file.type.includes('image')){
-            document.getElementById('message').innerText = 'Некорректный тип файлов';
-            return;
+    let fd = new FormData();
+    let nameErrorArr = [];
+    let sizeErrorArr = [];
+    let typeCheckArr = [];
+    for (let file of this.elements['foto[]'].files){
+        console.log(file);
+        if (file.size >= 2097152) {
+            sizeErrorArr.push(file)
+            continue;
+        }
+        if (!file.type.includes('image')) {
+            typeCheckArr.push(file)
+            continue;
         }
         if (!file.name.trim().length){
-            document.getElementById('message').innerText = 'Некорректное имя фалов';
-            return;
+            nameErrorArr.push(file);
+            continue;
         }
-        if (maxSize <20971520 ) document.getElementById('message').innerText = 'Файлы отправленны на сервер'
-        else document.getElementById('message').innerText = 'Некорректный размер файлов'
+        fd.append('foto[]',file);
     }
-})
+    if (nameErrorArr.length){
+        document.getElementById('name-message')
+        .innerText = `Из- за ошибки в имени файла небыло отправленно ${nameErrorArr.length} 
+        файлов(а)`
+    }
+    if (sizeErrorArr.length){
+        document.getElementById('size-message')
+            .innerText = `Из - за ошибки с размером файла небыло отправленно ${sizeErrorArr.length} 
+        файлов(а)`
+    }
+    if (typeCheckArr.length){
+        document.getElementById('type-message')
+            .innerText = `Из- за ошибки с типом файла небыло отправленно ${typeCheckArr.length} файлов(а)`
+        /*     .innerHTML = `
+        <p>Из- за ошибки с типом файла небыло отправленно ${typeCheckArr.length} 
+        файлов(а)</p>`
+       <button type="button" id="full-type">Подробнее</button>
+        document.getElementById('full-type').addEventListener('click',function () {
+           for (let error of typeCheckArr){
+               let p =document.createElement('p');
+                  p.innerHTML =`<p>${error.name}</p>`
+               let oldP= document.getElementById('type-error');
+              document.insertBefore (p,oldP)
+           }*/
+
+        }
+       fetch('/php/server-check.php', {
+        method: 'post',
+        body: fd
+    })
+        .then(response => response.text())
+        .then(text => {
+            console.log(text);
+            document.getElementById('response').innerText = text;
+        });
+});
--- hw3/php/server-check.php
+++ hw3/php/server-check.php
@@ -1,1 +1,24 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET') header('Location: /index.html');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_FILES['foto'])) {
+        $count = count($_FILES['foto']['name']);
+        for ($i = 0; $i < $count; $i += 1) {
+            $file_name = $_FILES['foto']['name'][$i];
+            $tmp_name = $_FILES['foto']['tmp_name'][$i];
+            if ($_FILES['foto']['error'][$i] == 0) {
+                if ($_FILES['foto']['size'][$i] < 2097152) {
+                    $string = $_FILES['foto']['type'][$i];
+                    $check = strpos($string, 'image');
+                    if ($check !== false) {
+                        $full_file_name = microtime().$file_name;
+                        $move_result = move_uploaded_file($tmp_name,"../img/$full_file_name");
+                        if ($move_result) echo "Файл $file_name отправлен на сервер \n";
+                    }else echo "$file_name не прошёл проверку типа файла";
+                }else echo "$file_name слишком велик для загрузки";
+            }else echo "Ошибка при загрузке файла $file_name на сервер";
+        }
+    }
+}
+
+
